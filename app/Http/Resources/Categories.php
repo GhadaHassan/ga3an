16e1a4d50af87379c3288ec0e1058425427d5e75
Majdy <mohamedmagdy2332@gmail.com>
2018-02-13 23:19:36 +0200

    Category Module

--- app/Http/Resources/Categories.php
+++ app/Http/Resources/Categories.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class Categories extends ResourceCollection
+class Categories extends Resource
 {
     /**
      * Transform the resource collection into an array.
